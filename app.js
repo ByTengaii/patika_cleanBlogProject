e146112cd04b8686106011e410d9154e33b61d5f
gagir <agirgurkan06@gmail.com>
2023-09-21 10:43:37 +0300

    feat: ejs apply

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+const ejs =  require('ejs');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (res, req) => {
-    const blog = {
-        id : 1,
-        tittle : "Blog tittle",
-        description : "Blog description"
-    }
-    req.send(blog);
-})
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
+});
 
 app.listen(port,() => {
     console.log(`Server start at ${port}`);
