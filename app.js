a35fb88c2cc2704024ffcb622c1f2cc8ba0b5956
gagir <agirgurkan06@gmail.com>
2023-09-24 04:29:41 +0300

    feat: mongoose usage and dynamic index is done.

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const ejs =  require('ejs');
 const express = require('express');
+const mongoose = require('mongoose');
+const Post = require('./models/Post.js');
 const app = express();
 const port = 3000;
 
+//* Middle Wares
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.render('index');
+//* MongoDB connection
+const connection = mongoose.connect('mongodb://localhost/cleanblog-test-db');
+
+connection.catch(err => {
+    if (err) console.log(err);
+});
+
+app.get('/', async (req, res) => {
+    const posts = await Post.find({}).exec();
+    res.render('index',{
+        posts
+    });
 });
 
 app.get('/about', (req, res) => {
@@ -16,6 +31,12 @@ app.get('/about', (req, res) => {
 
 app.get('/add_post', (req, res) => {
     res.render('add_post');
+});
+
+app.post('/post', async (req,res) => {
+    await Post.create(req.body);
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.listen(port,() => {
