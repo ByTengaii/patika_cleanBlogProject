b7ddb9344543d4e910ecf90ea8edba73b6b0be44
gagir <agirgurkan06@gmail.com>
2023-10-01 21:25:45 +0300

    feat: update, delete button added and work

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const ejs =  require('ejs');
 const express = require('express');
 const mongoose = require('mongoose');
 const Post = require('./models/Post.js');
+const methodOverride = require('method-override')
+
 const app = express();
 const port = 3000;
 
@@ -10,6 +12,9 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(methodOverride('_method',{
+    methods:['GET', 'POST']
+}));
 
 //* MongoDB connection
 const connection = mongoose.connect('mongodb://localhost/cleanblog-test-db');
@@ -19,7 +24,7 @@ connection.catch(err => {
 });
 
 app.get('/', async (req, res) => {
-    const posts = await Post.find({}).exec();
+    const posts = await Post.find({}).sort('-dataCreated').exec();
     res.render('index',{
         posts
     });
@@ -39,12 +44,30 @@ app.get('/posts/:id', async (req,res) => {
         post
     });
 })
-app.post('/post', async (req,res) => {
+app.post('/posts', async (req,res) => {
     await Post.create(req.body);
-    console.log(req.body);
     res.redirect('/');
 });
 
+app.get('/posts/edit/:id', async (req,res) => {
+    const post = await Post.findById(req.params.id).exec();
+    res.render('editPost',{
+        post
+    });
+});
+
+app.put('/posts/:id', async (req,res) => {
+    const post = await Post.findById(req.params.id);
+    post.title = req.body.title;
+    post.detail = req.body.detail;
+    post.save();
+    res.redirect(`/posts/${req.params.id}`);    
+});
+
+app.delete('/posts/:id', async (req, res) => {
+    await Post.findByIdAndRemove(req.params.id);
+    res.redirect('/');
+})
 
 app.listen(port,() => {
     console.log(`Server start at ${port}`);
